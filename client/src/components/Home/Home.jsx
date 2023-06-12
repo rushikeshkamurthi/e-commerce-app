4288347a4ca3253be12ead27962a36d48718b0e5
rushikeshkamurthi <k.hrushikesh007@gmail.com>
2023-06-12 21:41:56 +0530

    Products section implemented

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Category from "../Category/Category";
+import Products from "../Products/Products";
 import Banner from "./Banner/Banner";
 import "./Home.scss";
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div className="main-content">
             <div className="layout">
                 <Category />
+                <Products/>
             </div>
         </div>
 
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,6 +1,12 @@
+import Product from "./Product/Product";
 import "./Products.scss";
 const Products = () => {
-    return <div>Products</div>;
+    return <div className="products-container">
+        <div className="sec-heading">Popular Products</div>
+            <div className="products">
+            <Product/><Product/><Product/><Product/><Product/><Product/><Product/><Product/>
+            </div>
+    </div>;
 };
 
 export default Products;
